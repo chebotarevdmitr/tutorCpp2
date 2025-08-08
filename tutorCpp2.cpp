a2610817f37ee803da6a5e1ff1ed0dd9b3603079
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-08-08 21:35:57 +0300

    Добавьте файлы проекта.

---
+++ tutorCpp2.cpp
@@ -0,0 +1,13 @@
+﻿// tutorCpp2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::cout << "привет калькулятор\n";
+	system("pause<0");
+	return 0;
+}
