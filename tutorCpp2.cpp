edfb6908d484d54f4969cc8e4685bf362e1e6952
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-08-08 22:01:11 +0300

    калькулятор

--- tutorCpp2.cpp
+++ tutorCpp2.cpp
@@ -8,8 +8,39 @@ int main()
 	setlocale(LC_ALL, "Russian");
 
 	std::cout << "привет калькулятор\n";
+	float number1, number2;
+	std::cout << "введите первое число: ";
+	std::cin >> number1;
+	std::cout << "введите второе число: ";
+	std::cin >> number2;
 
-
+	char operation;
+	std::cout << "введите операцию (+, -, *, /): ";
+	std::cin >> operation;
+	float result;
+	if (operation == '+') {
+		result = number1 + number2;
+	}
+	else if (operation == '-') {
+		result = number1 - number2;
+	}
+	else if (operation == '*') {
+		result = number1 * number2;
+	}
+	else if (operation == '/') {
+		if (number2 != 0) {
+			result = number1 / number2;
+		}
+		else {
+			std::cout << "Ошибка: деление на ноль!" << std::endl;
+			return 1; // Завершение программы с ошибкой
+		}
+	}
+	else {
+		std::cout << "Неизвестная операция!" << std::endl;
+		return 1; // Завершение программы с ошибкой
+	}
+	std::cout << "Результат: "<<number1 <<" "<<operation<<" "<<number2 <<" = " << result << std::endl;
 
 	system("pause>0");
 	return 0;
