abd373de8e9f9a8fae614159067fed766a7fd588
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-09-14 23:08:31 +0300

    modifa

--- calculete.cpp
+++ calculete.cpp
@@ -1,47 +1,47 @@
-﻿// tutorCpp2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿//// tutorCpp2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
-
-int main()
-{
-	setlocale(LC_ALL, "Russian");
-
-	std::cout << "привет калькулятор\n";
-	float number1, number2;
-	std::cout << "введите первое число: ";
-	std::cin >> number1;
-	std::cout << "введите второе число: ";
-	std::cin >> number2;
-
-	char operation;
-	std::cout << "введите операцию (+, -, *, /): ";
-	std::cin >> operation;
-	float result;
-	if (operation == '+') {
-		result = number1 + number2;
-	}
-	else if (operation == '-') {
-		result = number1 - number2;
-	}
-	else if (operation == '*') {
-		result = number1 * number2;
-	}
-	else if (operation == '/') {
-		if (number2 != 0) {
-			result = number1 / number2;
-		}
-		else {
-			std::cout << "Ошибка: деление на ноль!" << std::endl;
-			return 1; // Завершение программы с ошибкой
-		}
-	}
-	else {
-		std::cout << "Неизвестная операция!" << std::endl;
-		return 1; // Завершение программы с ошибкой
-	}
-	std::cout << "Результат: "<<number1 <<" "<<operation<<" "<<number2 <<" = " << result << std::endl;
-
-	system("pause>0");
-	return 0;
-}
+//#include <iostream>
+//
+//int main()
+//{
+//	setlocale(LC_ALL, "Russian");
+//
+//	std::cout << "привет калькулятор\n";
+//	float number1, number2;
+//	std::cout << "введите первое число: ";
+//	std::cin >> number1;
+//	std::cout << "введите второе число: ";
+//	std::cin >> number2;
+//
+//	char operation;
+//	std::cout << "введите операцию (+, -, *, /): ";
+//	std::cin >> operation;
+//	float result;
+//	if (operation == '+') {
+//		result = number1 + number2;
+//	}
+//	else if (operation == '-') {
+//		result = number1 - number2;
+//	}
+//	else if (operation == '*') {
+//		result = number1 * number2;
+//	}
+//	else if (operation == '/') {
+//		if (number2 != 0) {
+//			result = number1 / number2;
+//		}
+//		else {
+//			std::cout << "Ошибка: деление на ноль!" << std::endl;
+//			return 1; // Завершение программы с ошибкой
+//		}
+//	}
+//	else {
+//		std::cout << "Неизвестная операция!" << std::endl;
+//		return 1; // Завершение программы с ошибкой
+//	}
+//	std::cout << "Результат: "<<number1 <<" "<<operation<<" "<<number2 <<" = " << result << std::endl;
+//
+//	system("pause>0");
+//	return 0;
+//}
---
+++ next.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name{"Dmitriy"};
+
+std::cout<<"Hello, " << name << "!\n";
+std::cout << "How are you today?\n";
+return 0;
+}
