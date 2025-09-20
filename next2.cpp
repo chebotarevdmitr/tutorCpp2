7873251c266592c896c3184532176d9fa8fe1eac
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-09-20 20:29:13 +0300

    new file:   next2.cpp  next3.cpp

---
+++ next2.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//
+//int main() {
+//	int number = 345;
+//	std::cout << "number = " << number << std::endl;// Вывод значения переменной number value of number
+//	std::cout << &number << "  input adress" << std::endl; // Вывод адреса переменной number address of number
+//
+//	int array[5] = { 1, 2, 3, 4, 5 };
+//	std::cout << "arrray = ";
+//	for (int i = 0; i < 5; i++) {
+//		std::cout << array[i] << " ";
+//	}
+//    	std::cout << std::endl;
+//		std::cout<<"adress of array[0] = "<<&array[2]<<std::endl;
+//		std::cout<<&array[2]<<std::endl;
+//	std::cout << "array[2] = " << array[2] << std::endl; 
+//return 0;
+//}
