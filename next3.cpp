b93bdb2a55373b87ff24d7b2254ed11faef602fc
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-09-21 11:22:56 +0300

    added next3.cpp next4_array.cpp

---
+++ next3.cpp
@@ -0,0 +1,15 @@
+// 
+#include <iostream>
+
+int x = 18;
+int main() {
+	int y = 2;
+	std::cout << x + 3 * y << std::endl;
+    std::cout << x + (3 * y) << std::endl;
+    std::cout << (x + 3) * y << std::endl;
+    std::cout << (x + 3) * (y + 1) << std::endl;
+	std::cout << ::x << std::endl;
+
+
+	return 0;
+}
---
+++ next4_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <array>
+#include <algorithm>
+
+int main() {
+
+
+
+
+	return 0;
+}
